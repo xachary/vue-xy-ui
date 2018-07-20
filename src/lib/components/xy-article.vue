71482ee278c3509065a0f02c48517055856184b2
xachary <xachary@qq.com>
2018-07-20 17:45:19 +0800

    article

--- src/lib/components/xy-article.vue
+++ src/lib/components/xy-article.vue
@@ -20,14 +20,67 @@
         that.update()
       }
     },
+    computed: {
+      html () {
+        let that = this
+        return that.array.join('')
+      }
+    },
     methods: {
+      replace (txt) {
+        return txt.replace(/(<img.*?)(src=")(.*?)(".*?>)/g, `<div class="lazy-load">$1v-lazy="'$3'$4</div>`)
+      },
       update () {
         let that = this
         if (that.des) {
-          sessionStorage.setItem('A', that.des)
-          sessionStorage.setItem('B', that.des.replace(/(<img.*?)(src=")(.*?)(".*?>)/g, '<div class="lazy-load">$1v-lazy="\'$3\'$4</div>'))
-          sessionStorage.setItem('C', '/(<img.*?)(src=")(.*?)(".*?>)/g')
-          sessionStorage.setItem('D', '<div class="lazy-load">$1v-lazy="\'$3\'$4</div>')
+          //          let start = 0
+          //          that.array = []
+          //          let ms = that.des.match(/(<img .*?>)/g)
+          //          for (let i = 0; i < ms.length; i++) {
+          //            let lc = that.des.indexOf(ms[i], start)
+          //            if (start !== lc) {
+          //              that.array.push(that.des.substr(start, lc))
+          //            }
+          //            that.array.push(that.replace(that.des.substr(lc, ms[i].length)))
+          //            start = lc + ms[i].length
+          //          }
+          //          if (start < that.des.length) {
+          //            that.array.push(that.replace(that.des.substring(start, that.des.length)))
+          //          }
+
+          //          console.log(that.array.map(function (item) {
+          //            return item.value
+          //          }))
+          //          let ta = tmp.split('')
+          //          for (let i = 0; i < ms.length; i++) {
+          //            let lc = tmp.indexOf(ms[i])
+          //            let txt = ''
+          //            if (lc > 0) {
+          //              //              debugger
+          //              txt = ta.splice(0, lc)
+          //                      .join('')
+          //              that.array.push({
+          //                type: 'txt',
+          //                value: txt
+          //              })
+          //              tmp = tmp.replace(txt, '')
+          //            }
+          //            //            debugger
+          //            txt = ta.splice(0, ms[i].length)
+          //                    .join('')
+          //            that.array.push({
+          //              type: 'img',
+          //              value: txt
+          //            })
+          //            tmp = tmp.replace(txt, '')
+          //          }
+          //          if (tmp) {
+          //            that.array.push({
+          //              type: 'txt',
+          //              value: tmp
+          //            })
+          //          }
+
           var text = Vue.extend({
             template: `<div>${that.des.replace(/(<img.*?)(src=")(.*?)(".*?>)/g, '<div class="lazy-load">$1v-lazy="\'$3\'$4</div>')}</div>`
           })
@@ -36,7 +89,9 @@
       }
     },
     data () {
-      return {}
+      return {
+        array: []
+      }
     },
     mounted: function () {
       let that = this
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,5 @@
 import Style from './scss/style.scss'
 
-import xyReachBottom from './components/xy-reach-bottom.vue'
-import xyScaleSlider from './components/xy-scale-slider.vue'
-import xyListStatus from './components/xy-list-status.vue'
-import xyArticle from './components/xy-article.vue'
-
 import xyKeepTop from './directives/xy-keep-top'
 import xyMovingBtn from './directives/xy-moving-btn'
 import xyPullRefresh from './directives/xy-pull-refresh'
@@ -12,18 +7,24 @@ import xyPullRefresh from './directives/xy-pull-refresh'
 import xyLazyloadResize from './plugins/xy-lazyload-resize'
 import VueLazyload from 'vue-lazyload'
 
+import xyReachBottom from './components/xy-reach-bottom.vue'
+import xyScaleSlider from './components/xy-scale-slider.vue'
+import xyListStatus from './components/xy-list-status.vue'
+import xyArticle from './components/xy-article.vue'
+
 const xyUI = {
   install (Vue) {
-    Vue.component(xyReachBottom.name, xyReachBottom)
-    Vue.component(xyScaleSlider.name, xyScaleSlider)
-    Vue.component(xyListStatus.name, xyListStatus)
-    Vue.component(xyArticle.name, xyArticle)
-
     Vue.directive(xyKeepTop.name, xyKeepTop)
     Vue.directive(xyMovingBtn.name, xyMovingBtn)
     Vue.directive(xyPullRefresh.name, xyPullRefresh)
 
     Vue.use(VueLazyload, xyLazyloadResize)
+
+    // 尽量按依赖关系按顺序注册
+    Vue.component(xyReachBottom.name, xyReachBottom)
+    Vue.component(xyScaleSlider.name, xyScaleSlider)
+    Vue.component(xyListStatus.name, xyListStatus)
+    Vue.component(xyArticle.name, xyArticle)
   }
 }
 
