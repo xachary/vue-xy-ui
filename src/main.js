f46c3b33e1b5ba4296d9b0f4803da1f3d3adadc7
xachary <xachary@qq.com>
2018-10-31 13:55:22 +0800

    iphonex兼容调整

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import PageProgress from './page-progress.vue'
+import IphoneX from './iphone-x.vue'
+import Media from './media.vue'
 import Style from './lib/scss/style.scss'
 
 const routes = [
@@ -9,12 +11,21 @@ const routes = [
     path: '/page-progress',
     name: 'page-progress',
     component: PageProgress
+  },
+  {
+    path: '/iphone-x',
+    name: 'iphone-x',
+    component: IphoneX
+  },
+  {
+    path: '/media',
+    name: 'media',
+    component: Media
   }
 ]
 const router = new VueRouter({
   routes // (缩写) 相当于 routes: routes
 })
-
 new Vue({
   router,
   el: '#app',
